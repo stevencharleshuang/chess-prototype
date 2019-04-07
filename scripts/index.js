580c7ba2054e7ca81aa38376740f208c7bf863af
Steven Huang <steven.charles.huang@gmail.com>
2019-04-06 22:22:26 -0400

    board squares are built with alphanumeric data-id

--- scripts/index.js
+++ scripts/index.js
@@ -5,18 +5,28 @@ $(document).ready(() => {
   // Make the board
   const createBoard = () => {
     let prevWhite = false;
+    let numNum = 8;
+    let alphaNum = 97;
 
     for (let i = 0; i < 64; i += 1) {
+      if (alphaNum > 104) {
+        alphaNum = 97;
+      }
+
       if (!prevWhite) {
-        $gameArea.append(`<div class="board-square white-square" data-num="${i}" data-id></div>`);
+        $gameArea.append(`<div class="board-square white-square" data-num="${i}" data-id="${String.fromCharCode(alphaNum)}${numNum}"></div>`);
         prevWhite = true;
       } else {
-        $gameArea.append(`<div class="board-square black-square" data-num="${i}" data-id></div>`);
+        $gameArea.append(`<div class="board-square black-square" data-num="${i}" data-id="${String.fromCharCode(alphaNum)}${numNum}"></div>`);
         prevWhite = false;
       }
+
       if (i % 8 === 7) {
         prevWhite = !prevWhite;
+        numNum -= 1;
       }
+
+      alphaNum += 1;
     }
   }
   
